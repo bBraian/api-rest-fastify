a13c12f3e940711b39fdf3d1393518936221dd69
bBraian <braianvoficial@gmail.com>
2024-01-24 12:55:41 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+Projeto desenvolvido durante o curso de NodeJS da @Rocketseat
+
+Conceitos trabalhados
+*Typescript
+*Fastify
+*EsLint
---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import { knex } from './database'
+
+const app = fastify()
+
+app.get('/hello', async () => {
+  const test = await knex('sqlite_schema').select('*')
+  return test
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('Server running')
+  })
+  .catch((err) => {
+    console.log(err, 'Server failed')
+  })
